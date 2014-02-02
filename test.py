16fa16da6f77bb39375a4d567ee02d524b8bbfd5
hongboso <hongboso@umich.edu>
2014-02-02 16:17:32 -0500

    Me, Yao, and Diana work together, write sample code in document Jay provide us in python version

--- test.py
+++ test.py
@@ -22,14 +22,8 @@ def load_sections(filename):
     index += 1
   index = 0
   
-  is_firstline = True
   for line in region_file:
-    if is_firstline:
-      is_firstline = False
-      continue
-
     fields = line.split(";;|")
-
     #add each line of log files into each fields list
     for field in fields:
       fields_dict[fields_name[index]].append(field) # field
@@ -40,19 +34,65 @@ def load_sections(filename):
   return
 
 
+# list dictionary in list mode
 def print_dict_list_mode(my_dict):
   for field in fields_name:
     print field, ": " ,my_dict[field]
   return
 
+ 
+def print_requests_per_hour(my_dict):
+  requests_per_hour = dict()
+  for i in range(0, 24):
+    requests_per_hour[i] = 0 
+  
+  for my_timestamp in my_dict["timestamps"]:
+    x = int(my_timestamp.split(" ")[3].split(":")[0])
+    requests_per_hour[x] += 1
+  
+  for requests_index in requests_per_hour:
+    print requests_index, requests_per_hour[requests_index]
+
+  return
+
+
+def print_avg_time_view_doc(my_dict):
+  avg_time_info = dict()
+
+  for i in range(0, len(my_dict)):
+    my_hostname = fields_dict["hostname"][i]
+    if my_hostname == "":
+      continue
+
+    if my_hostname in avg_time_info:
+    
+      add_time = 0
+      if fields_dict["response_time"][i] != "":
+        add_time = int(fields_dict["response_time"][i])
+        
+      avg_time_info[my_hostname][0] += add_time
+      avg_time_info[my_hostname][1] += 1 
+    else:
+      temp_info = []
+      init_time = 0
+      if fields_dict["response_time"][i] != "":
+        init_time = int(fields_dict["response_time"][i])
+      temp_info.append(init_time)
+      temp_info.append(1)
+      avg_time_info[my_hostname] = temp_info
+
+  
+  for item in avg_time_info:
+    print item, float(avg_time_info[item][0])/avg_time_info[item][1], avg_time_info[item][1]
+  
+  return
+
 
 def main():
-  load_sections("log1.txt")
-  #print_dict(fields_dict)
+  load_sections("sample_logfile.txt")
 
-  print_dict_list_mode(fields_dict)
-
+  #print_avg_time_view_doc(fields_dict["timestamps"])
+  #print_requests_per_hour(fields_dict)
+  #print_dict_list_mode(fields_dict)
 
 main();
-
-#test
