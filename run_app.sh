61742b916c008a54ded2464dfddc135b62a01920
hongboso <hongboso@umich.edu>
2014-02-15 18:51:09 -0500

    small fix

--- run_app.sh
+++ run_app.sh
@@ -1,12 +1,12 @@
-python /home/ec2-user/UMMDP14/app_server_parse.py 
+python app_server_parse.py 
 /home/ec2-user/ummdp/logfiles/101/app/2013/* 
 /home/ec2-user/ummdp/logfiles/101/app/2014/* 
-> /home/ec2-user/UMMDP14/app_server_output/app_server_101.out;
-python /home/ec2-user/UMMDP14/app_server_parse.py 
+> /app_server_output/app_server_101.out;
+python app_server_parse.py 
 /home/ec2-user/ummdp/logfiles/102/app/2013/* 
 /home/ec2-user/ummdp/logfiles/102/app/2014/*
 > /home/ec2-user/UMMDP14/app_server_output/app_server_102.out;
-python /home/ec2-user/UMMDP14/app_server_parse.py
+python app_server_parse.py
 /home/ec2-user/ummdp/logfiles/103/app/2013/* 
 /home/ec2-user/ummdp/logfiles/103/app/2014/* 
 > /home/ec2-user/UMMDP14/app_server_output/app_server_103.out;
