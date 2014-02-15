b2b6e48dab514aafdd3990fecb026545c274edc1
hongboso <hongboso@umich.edu>
2014-02-15 18:49:34 -0500

    seperate server

---
+++ run_apache.sh
@@ -0,0 +1,8 @@
+python /home/ec2-user/UMMDP14/apache_parse.py 
+/home/ec2-user/ummdp/logfiles/101/apache_access/2013/* 
+/home/ec2-user/ummdp/logfiles/101/apache_access/2014/* 
+/home/ec2-user/ummdp/logfiles/102/apache_access/2013/* 
+/home/ec2-user/ummdp/logfiles/102/apache_access/2014/* 
+/home/ec2-user/ummdp/logfiles/103/apache_access/2013/* 
+/home/ec2-user/ummdp/logfiles/103/apache_access/2014/* 
+> /home/ec2-user/UMMDP14/apache_access_server_output/apache_access_server.out
