75b00a57f7add7315551928a6123ca1193e819db
EC2 Default User <ec2-user@ip-10-30-171-37.ec2.internal>
2014-02-16 17:35:27 +0000

    apache_output

--- parse.sh
+++ parse.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-dir1='/home/ec2-user/ummdp/logfiles/101/apache_access/2013'
-dir2='/home/ec2-user/ummdp/logfiles/102/apache_access/2013'
-dir3='/home/ec2-user/ummdp/logfiles/103/apache_access/2013'
+dir1='/home/ec2-user/ummdp/logfiles/101/apache_access/2014/'
+dir2='/home/ec2-user/ummdp/logfiles/102/apache_access/2014/'
+dir3='/home/ec2-user/ummdp/logfiles/103/apache_access/2014/'
 dir='/home/dianazh/mdp/logfiles/UMMDP14/logfiles'
 appname='elibdmz-elibweb_usage.log.2013-'
-servername='elibrary.bigchalk.com-access_log.13'
+servername='elibrary.bigchalk.com-access_log.14'
 month=0
 end='*'
 app=''
 serv=''
 appout='result/app_out_2013_'
-servout='result/serv_out_2013_'
-cd $dir
-for ((i=0;i<12;i++)) do
+servout='result/2014/serv_out_2013_'
+#cd $dir
+for ((i=0;i<2;i++)) do
     month=$(($i+1))
 #app=$appname$(printf '%02d' $month)$end
 #python app_server_parse.py $dir1$app $dir2$app $dir3$app > $appout$(printf '%02d' $month)
