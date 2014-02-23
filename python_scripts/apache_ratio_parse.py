723244537428f799552d5177fd16874a35d4bf5d
hongboso <hongboso@umich.edu>
2014-02-22 19:33:16 -0500

    change back python script state

--- python_scripts/apache_ratio_parse.py
+++ python_scripts/apache_ratio_parse.py
@@ -101,7 +101,7 @@ def main():
     '''
     file13 = 'elibrary.bigchalk.com-access_log.13'
     file14 = 'elibrary.bigchalk.com-access_log.14'
-    '''
+    
     for x in range(1,13):
         month = "%02d" %(x)
         for y in range (1,32):
@@ -109,15 +109,15 @@ def main():
                 file1 = dir1+file13+month+date
                 file2 = dir2+file13+month+date
                 file3 = dir3+file13+month+date
-                
+                '''
                 file4 = dirh1+file14+month+date
                 file5 = dirh2+file14+month+date
                 file6 = dirh3+file14+month+date
-                
+                '''
                 if (os.path.exists(file1) and os.path.exists(file2) and os.path.exists(file3)):
                         load_a_day(file1,file2,file3)
                         print_expand_result('13',month,date)
-    '''
+    
 
     for x in range(1,13):
         month = "%02d" %(x)
