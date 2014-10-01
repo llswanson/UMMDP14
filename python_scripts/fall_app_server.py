307746b4310ba9acaa444fbcfa2dcc3c3da0db57
dianazh <dianazh@umich.edu>
2014-10-01 02:12:12 +0000

    fix bug: mem outage problem

--- python_scripts/fall_app_server.py
+++ python_scripts/fall_app_server.py
@@ -2,6 +2,7 @@ import sys
 import csv
 import re
 import os
+# import pdb
 
 fields_name = ["timestamps", "program_name", "product_SKU", "request_type", "request_url", "client_ip", "query", "request_service", "resource_type",
                "resource_id", "description", "result_count", "response_code", "response_time", "user_id", "user_account", "group_account",
@@ -97,7 +98,6 @@ def get_session_preview_counts(my_dict):
 
     urls = my_dict[session_id]["request_url"]
     types = my_dict[session_id]["request_service"]
-    #for url in urls:
     for i in range (0,len(urls)):
       search_url1 = "/do/document"
       search_url2 = "SEARCH_DOC_RETRIEVAL;;DocumentPreview"
@@ -159,7 +159,8 @@ def load(file):
   session_retrieval_table = get_session_retrieval_counts(session_dict)
   session_preview_table = get_session_preview_counts(session_dict)
   session_search_table = get_session_search_counts(session_dict)
-
+  session_dict.clear()
+    
   for session in session_search_table:
     session_table[session] = dict()
     session_table[session]["search"] = session_search_table[session]
@@ -170,27 +171,33 @@ def load(file):
   for session in session_retrieval_table:
     session_table[session]["retrieval"] = session_preview_table[session]
 
+  session_search_table.clear()
+  session_retrieval_table.clear()
+  session_preview_table.clear()
+
   return session_table
 
 def combine_session_table(session_table_main, session_table_to_add):
-    for key, value in session_table_to_add
+    for key, value in session_table_to_add.iteritems():
         session_table_main[key] = value
+    session_table_to_add.clear()
 
 def main():
     file_list_prefix = "/home/ec2-user/UMMDP14/2014_d/app_server" 
     app_server= dict()
     dates = set()
-    for i in range(101, 115):
-        file_list_name = file_list_prefix + i
+    for i in range(101, 116):
+        file_list_name = file_list_prefix + str(i)
         app_server[i] = dict()
         app_server[i]['file_list'] = open(file_list_name, 'rU')
         app_server[i]['files'] = dict()
         for filename in app_server[i]['file_list']:
+            filename = filename.rstrip('\n')
             date = filename.split(".")[2]
             dates.add(date)
             app_server[i]['files'][date] = filename
         app_server[i]['file_list'].close()
-            
+ 
     ordered_date = list(dates)
     ordered_date.sort()
 
@@ -198,21 +205,22 @@ def main():
     comma = ','
     newline = '\n'
     for date in ordered_date:
+        # write session table to a file
+        write_filename = write_file_prefix + date
+        output_file = open(write_filename, 'w+')
+        output_file.write("session_id,date,search,retrieval,preview," + newline)
+
         # get session table for a day
-        session_table_on_date = dict()
-        for i in range(101, 115):
+        for i in range(101, 116):
             path = app_server[i]['files'][date]
             if (os.path.exists(path)):
                 session_table_for_server_i = load(path) 
-            combine_session_table(session_table_on_date, session_table_for_server_i)
-        # write session table to a file
-        write_filename = write_file_prefix + date
-        output_file = open(write_filename, 'r+')
-        output_file.write("session_id,date,search,retrieval,preview,")
-        for key, value in session_table_on_date:
-            output_file.write(key + comma + date + comma + value['search'] + comma + value['retrieval'] + comma + value['preview'] + comma + newline)
+            for key, value in session_table_for_server_i.iteritems():
+                output_file.write(key + comma + date + comma + str(value['search']) + comma + str(value['retrieval']) + comma + str(value['preview']) + comma + newline)
+            session_table_for_server_i.clear()
         output_file.close()
-return
+    
+    return
 
 main();
 
