5d58387003829df38a46cffd399130d488d613a9
dianazh <dianazh@umich.edu>
2014-10-23 09:36:15 +0000

    rewrite the file, with condensed structure, add export_easylib feature, filter out internal ip, fix session_id in url bug

--- python_scripts/fall_app_server.py
+++ python_scripts/fall_app_server.py
@@ -2,7 +2,8 @@ import sys
 import csv
 import re
 import os
-# import pdb
+import urllib
+import pdb
 
 fields_name = ["timestamps", "program_name", "product_SKU", "request_type", "request_url", "client_ip", "query", "request_service", "resource_type",
                "resource_id", "description", "result_count", "response_code", "response_time", "user_id", "user_account", "group_account",
@@ -11,21 +12,35 @@ fields_name = ["timestamps", "program_name", "product_SKU", "request_type", "req
                "delivery_format", "document_source", "dumy1", "content_type", "authentication_method", "language", "interface_type", "dumy2",
                "subscription_id", "usage_type", "user_agent"]
 
-file_size = 0
-session_dict = dict()
-session_table = dict()
-
+# generate dict: 
+#   session_id : dict of (field_name : list of values)
 def load_sections(filename):
+  session_dict = dict()
   region_file = open(filename, 'rU')
   index = 0
   
   for line in region_file:
     index = 0
     fields = line.split(";;|")
+    # filter out internal ip
+    ip = fields[5]
+    if (ip.find("165.215") != -1):
+        continue 
+
     if (len(fields) >= 20):
         session_id = fields[20]
     else: 
-        print "Error @ " + filename + line
+        # session_id may be in url
+        query_str = urllib.unquote(fields[6])
+        #delete the stirng that might separte a session id
+        query_str.replace("\n>","") 
+        ts_start = query_str.find("ts")
+        if (ts_start != -1):
+            ts_start += 3
+            session_id = query_str[ts_start:ts_start+32]
+        else:
+            print "Error @ " + filename + line
+  
     if session_id not in session_dict:
       session_dict[session_id] = dict()
 
@@ -37,179 +52,113 @@ def load_sections(filename):
       index += 1
 
   region_file.close()
-  return
+  return session_dict
 
-def get_session_search_counts(my_dict):
-  count = 0
-  session_search_counts = {}
-  for session_id in my_dict:
-    if session_id not in session_search_counts:
-      session_search_counts[session_id] = 0
+def load(file):
+    session_dict = load_sections(file)
+    session_table = dict()
+    for session_id in session_dict:
+        if session_id not in session_table:
+            session_table[session_id] = dict()  
+        urls = session_dict[session_id]["request_url"]
+        types = session_dict[session_id]["request_service"]
+        queries = session_dict[session_id]["query"]
+        get_features(session_table[session_id], urls, types, queries)
+    return session_table    
 
-    urls = my_dict[session_id]["request_url"]
-    types = my_dict[session_id]["request_service"]
+def get_features(session_table_perid, urls, types, queries):
+    # pdb.set_trace()
+    session_table_perid['search'] = get_session_search_counts(urls, types, queries)
+    session_table_perid['preview'], session_table_perid['preview_from_search'] = get_session_preview_counts(urls, types, queries) 
+    session_table_perid['retrieval'], session_table_perid['retrieval_from_search'] = get_session_retrieval_counts(urls, types, queries)
+    session_table_perid['addtomylist'] = get_session_addtomylist_counts(urls, types, queries)
+    session_table_perid['email'] = get_session_email_counts(urls, types, queries)
+    session_table_perid['print'] = get_session_print_counts(urls,types, queries)
+    session_table_perid['export_easylib'] = get_export_easylib_counts(urls, types, queries)
+    return
+
+def get_session_search_counts(urls, types, queries):
     search_url1 = "/do/search"
     search_url2 = "SEARCH_SEARCH"
-      
+    
+    session_search_counts = 0
     for i in range (0,len(urls)):
       if urls[i].find(search_url1) != -1 and types[i].find(search_url2) != -1:
-        session_search_counts[session_id] += 1
+        session_search_counts += 1
+    return session_search_counts
 
-  return session_search_counts
-
-
-def get_session_retrieval_counts(my_dict):
-  count = 0
-  session_retrieval_counts = {}
-  session_retrieval_from_search = {}
-  for session_id in my_dict:
-    if session_id not in session_retrieval_counts:
-      session_retrieval_counts[session_id] = 0
-      session_retrieval_from_search[session_id] = 0
-
-    urls = my_dict[session_id]["request_url"]
-    types = my_dict[session_id]["request_service"]
-    queries = my_dict[session_id]["query"]
+def get_session_retrieval_counts(urls, types, queries):
     search_url1 = "/do/document"
     search_url2 = "SEARCH_DOC_RETRIEVAL;;Document"
     search_url3 = "set=search"
-    
+
+    session_retrieval_counts = 0
+    session_retrieval_from_search = 0
     for i in range (0,len(urls)):
-
       if urls[i].find(search_url1) != -1 and types[i].find(search_url2) != -1:
-        session_retrieval_counts[session_id] += 1
+        session_retrieval_counts += 1
         if queries[i].find(search_url3) != -1:
-            session_retrieval_from_search[session_id] += 1
+            session_retrieval_from_search += 1
+    return session_retrieval_counts, session_retrieval_from_search
 
-  return session_retrieval_counts, session_retrieval_from_search
-
-def get_session_preview_counts(my_dict):
-  count = 0
-  session_preview_counts = {}
-  session_preview_from_search = {}
-  for session_id in my_dict:
-    if session_id not in session_preview_counts:
-      session_preview_counts[session_id] = 0
-      session_preview_from_search[session_id] = 0
-
-    urls = my_dict[session_id]["request_url"]
-    types = my_dict[session_id]["request_service"]
-    queries = my_dict[session_id]["query"]
+def get_session_preview_counts(urls, types, queries):
     search_url1 = "/do/document"
     search_url2 = "SEARCH_DOC_RETRIEVAL;;DocumentPreview"
     search_url3 = "set=search"
 
+    session_preview_counts = 0
+    session_preview_from_search = 0
     for i in range (0,len(urls)):
       if urls[i].find(search_url1) != -1 and types[i].find(search_url2) != -1:
-        session_preview_counts[session_id] += 1
+        session_preview_counts += 1
         if queries[i].find(search_url3) != -1:
-            session_preview_from_search[session_id] += 1
+            session_preview_from_search += 1
+    return session_preview_counts, session_preview_from_search
 
-  return session_preview_counts, session_preview_from_search
-
-def get_session_addtomylist_counts(my_dict):
+def get_session_addtomylist_counts(urls, types, queries):
     # count all the files that are added to mylist
     # without specifying if is a regular doc or research topic
-    count = 0
-    session_addtomylist_counts = {}
-    for session_id in my_dict:
-        if session_id not in session_addtomylist_counts:
-           session_addtomylist_counts[session_id] = 0
-        urls = my_dict[session_id]["request_url"]
-        types = my_dict[session_id]["request_service"]
-        queries = my_dict[session_id]['query']
-        search_url1 = '/do/mylist'
-        search_url2 = 'MyListComponent'
-        search_url3 = 'mylist=add'
+    search_url1 = '/do/mylist'
+    search_url2 = 'MyListComponent'
+    search_url3 = 'mylist=add'
 
+    session_addtomylist_counts = 0
     for i in range (0, len(urls)):
         if urls[i].find(search_url1) != -1 and types[i].find(search_url2) != -1 and queries[i].find(search_url3) != -1:
-           session_addtomylist_counts[session_id] += 1
-
+           session_addtomylist_counts += 1
     return session_addtomylist_counts 
        
-def get_session_email_counts(my_dict):
-    count = 0
-    session_email_counts = {}
-    for session_id in my_dict:
-        if session_id not in session_email_counts:
-           session_email_counts[session_id] = 0
-        urls = my_dict[session_id]["request_url"]
-        queries = my_dict[session_id]['query']
-        search_url1 = '/do/email'
-        search_url2 = 'set=search'
+def get_session_email_counts(urls, types, queries):
+    search_url1 = '/do/email'
+    search_url2 = 'set=search'
 
+    session_email_counts = 0
     for i in range (0, len(urls)):
         if urls[i].find(search_url1) != -1 and queries[i].find(search_url2) != -1:
-           session_email_counts[session_id] += 1
-
+           session_email_counts += 1
     return session_email_counts 
 
-def get_session_print_counts(my_dict):
-    count = 0
-    session_print_counts = {}
-    for session_id in my_dict:
-        if session_id not in session_print_counts:
-           session_print_counts[session_id] = 0
-        urls = my_dict[session_id]["request_url"]
-        queries = my_dict[session_id]['query']
-        search_url1 = '/do/document'
-        search_url2 = 'set=search'
-        search_url3 = 'style=printable'
+def get_session_print_counts(urls, types, queries):
+    search_url1 = '/do/document'
+    search_url2 = 'set=search'
+    search_url3 = 'style=printable'
 
+    session_print_counts = 0
     for i in range (0, len(urls)):
         if urls[i].find(search_url1) != -1 and queries[i].find(search_url2) != -1 and queries[i].find(search_url3) != -1:
-           session_print_counts[session_id] += 1
-
+           session_print_counts += 1
     return session_print_counts 
- 
-def load(file):
-  session_table = dict()
-  load_sections(file)
-  session_retrieval_table, session_retrieval_from_search = get_session_retrieval_counts(session_dict)
-  session_preview_table, session_preview_from_search = get_session_preview_counts(session_dict)
-  session_search_table = get_session_search_counts(session_dict)
-  session_addtomylist_table = get_session_addtomylist_counts(session_dict)
-  session_email_table = get_session_email_counts(session_dict)
-  session_print_table = get_session_print_counts(session_dict)
-  session_dict.clear()
-    
-  for session in session_search_table:
-    session_table[session] = dict()
-    session_table[session]["search"] = session_search_table[session]
 
-  for session in session_preview_table:
-    session_table[session]["preview"] = session_preview_table[session]
+def get_export_easylib_counts(urls, types, queries):
+    search_url1 = '/do/exportToEasyBib'
+    search_url2 = 'ExportToEasyBibComponent;;ExportToEasyBib' 
 
-  for session in session_preview_from_search:
-    session_table[session]["preview_from_search"] = session_preview_from_search[session]
-
-  for session in session_retrieval_table:
-    session_table[session]["retrieval"] = session_retrieval_table[session]
-
-  for session in session_retrieval_from_search:
-    session_table[session]["retrieval_from_search"] = session_retrieval_from_search[session]
-
-  for session in session_addtomylist_table:
-    session_table[session]['addtomylist'] = session_addtomylist_table[session]
-
-  for session in session_email_table:
-    session_table[session]['email'] = session_email_table[session]
-
-  for session in session_print_table:
-    session_table[session]['print'] = session_print_table[session]
-
-  session_search_table.clear()
-  session_retrieval_table.clear()
-  session_preview_table.clear()
-  session_retrieval_from_search.clear()
-  session_preview_from_search.clear()
-  session_addtomylist_table.clear()
-  session_email_table.clear()
-  session_print_table.clear()
- 
-  return session_table
-
+    session_export_easylib_counts = 0
+    for i in range(0, len(urls)):
+        if urls[i].find(search_url1) != -1 and types[i].find(search_url2) != -1:
+            session_export_easylib_counts += 1
+    return session_export_easylib_counts
+         
 def main():
     file_list_prefix = "/home/ec2-user/UMMDP14/2014_d/app_server" 
     app_server= dict()
@@ -236,7 +185,8 @@ def main():
         # write session table to a file
         write_filename = write_file_prefix + date
         output_file = open(write_filename, 'w+')
-        header_str = "session_id,date,search,retrieval,retrieval_from_search,preview,preview_from_search,rs_ratio,add_to_my_list,email,print" 
+        header_str = "session_id,date,search,retrieval,retrieval_from_search,preview,preview_from_search,\
+                            rs_ratio,add_to_my_list,email,print,export_easylib," 
         output_file.write(header_str + newline)
 
         # get session table for a day
@@ -251,11 +201,16 @@ def main():
                 else:
                     total_retrieval_from_search =  value['retrieval_from_search'] + value['preview_from_search']
                     rs_ratio = total_retrieval_from_search / (value['search'] * 1.0) 
-                output_file.write(key + comma + date + comma + str(value['search']) + comma + str(value['retrieval']) + comma + str(value['retrieval_from_search']) + comma + str(value['preview']) + comma +str(value["preview_from_search"]) + comma + str(rs_ratio) + comma + str(value['addtomylist']) + comma + str(value['email']) + comma + str(value['print']) + comma + newline)
+                output_file.write(key + comma + date + comma + str(value['search']) + comma + \
+                                str(value['retrieval']) + comma + str(value['retrieval_from_search']) + comma + \
+                                str(value['preview']) + comma +str(value["preview_from_search"]) + comma + \
+                                str(rs_ratio) + comma + str(value['addtomylist']) + comma + \
+                                str(value['email']) + comma + str(value['print']) + comma + \
+                                str(value['export_easylib']) + comma + newline)
            
                  # for collection purpose
-                if (value['addtomylist'] != 0 or value['email'] != 0 or value['print'] != 0):
-                    print 'Find satisfaction point @ ' + key + comma + date + comma + str(value['search']) + comma + str(value['retrieval']) + comma + str(value['retrieval_from_search']) + comma + str(value['preview']) + comma +str(value["preview_from_search"]) + comma + str(rs_ratio) + comma + str(value['addtomylist']) + comma + str(value['email']) + comma + str(value['print']) + comma + newline
+                if (value['addtomylist'] != 0 or value['email'] != 0 or value['print'] != 0 or value['export_easylib'] != 0):
+                    print 'Find satisfaction point @ ' + key + comma + date + comma + str(value['search']) + comma + str(value['retrieval']) + comma + str(value['retrieval_from_search']) + comma + str(value['preview']) + comma +str(value["preview_from_search"]) + comma + str(rs_ratio) + comma + str(value['addtomylist']) + comma + str(value['email']) + comma + str(value['print']) + comma + str(value['export_easylib'])+ comma + newline
             
             session_table_for_server_i.clear()
         output_file.close()
