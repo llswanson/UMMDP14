874e8d034302d7b5142c6eca875defd614cb982a
dianazh <dianazh@umich.edu>
2014-11-12 03:40:06 +0000

    parse out training point by month

--- python_scripts/get_satis.py
+++ python_scripts/get_satis.py
@@ -3,14 +3,29 @@ import pdb
 
 def main():
     originfile = open('satis2014-10-23_22-00-48.log', 'rU')
-    output = open('satis_2day', 'w+')
-    i = 0
+    originfile2 = open('satis2014-11-12_00-47-14.log', 'rU')
+    filelist = ['date']
+    output = open(filelist[len(filelist)-1], 'w+')
     for line in originfile:
         fields = line.split(',')
+        month = fields[1][:7]
         #pdb.set_trace()
-        if fields[1] != '2013-01-01' and fields[1] != '2013-01-02':
-            continue
+        if month not in filelist[len(filelist)-1]:
+            output.close()
+            filelist.append(month)
+            output = open('satis/satis_'+month, 'w+')
         output.write(line)
+    originfile.close()
+    for line in originfile2:
+        fields = line.split(',')
+        month = fields[1][:7]
+        #pdb.set_trace()
+        if month not in filelist[len(filelist)-1]:
+            output.close()
+            filelist.append(month)
+            output = open('satis/satis_'+month, 'w+')
+        output.write(line)
+    originfile2.close()
     output.close()
     return
 main()
--- python_scripts/get_unsatis.py
+++ python_scripts/get_unsatis.py
@@ -2,29 +2,49 @@ import os, sys
 import pdb
 import time
 
-def main():
-    #path = '/home/ec2-user/UMMDP14/app_server_parsed'
-    path = '/home/ec2-user/UMMDP14/app_sp_test/'
+def get_by_date():
+    path = '/home/ec2-user/UMMDP14/app_server_parsed/'
+    #path = '/home/ec2-user/UMMDP14/app_sp_test/'
     dirs = os.listdir(path)
     dirs.sort()
-    output = open('unsatis'+time.strftime('%Y-%m-%d_%H-%M-%S')+'.log', 'w+')
     for filename in dirs:
-        first_line = True
         file = open(path + filename, 'rU')
+        output = open('unsatis_date/unsatis_' + filename, 'w+')
         for line in file:
             fields = line.split(',')
-            if first_line:
-                first_line = False
+            if len(fields) < 2:
+                print filename + line
                 continue
             #filter out entries that has no searches
-            elif int(fields[2]) == 0:
+            if 'search' in fields[2]:
+                continue
+            if int(fields[2]) == 0:
                 continue
             else:
                 #filter out users that has more search than retrieval
                 if float(fields[7]) < 1 and float(fields[8]) ==0 and float(fields[9]) == 0 and float(fields[10]) == 0 and float(fields[11]) == 0:
                     output.write(line)
+        output.close() 
         file.close()
-    output.close() 
     return
 
-main()
+def get_by_month():
+    path = '/home/ec2-user/UMMDP14/python_scripts/unsatis_date/' 
+    dirs = os.listdir(path)
+    dirs.sort()
+    filelist = ['null']
+    output = open('unsatis/unsatis_' + filelist[len(filelist)-1], 'a')
+    for filename in dirs:
+        if not filename[8:] in filelist[len(filelist)-1]:
+            output.close()
+            filelist.append(filename[8:-3])
+            #pdb.set_trace()
+            output = open('unsatis/unsatis_' + filelist[len(filelist)-1], 'a')
+        file = open(path + filename, 'rU')
+        output.write(file.read())
+
+def main():
+    get_by_date()
+    get_by_month()
+   
+main() 
